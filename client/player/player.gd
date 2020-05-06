485965d0ca01dca0c3a4e16dbf640d5c691a3535
Your Name <yang.official@gmail.com>
2020-05-05 19:39:45 -0700

    first commit

---
+++ client/player/player.gd
@@ -0,0 +1,18 @@
+extends KinematicBody2D
+
+# physics
+var speed = 200
+var jump = 600
+var gravity = 800
+var velocity = Vector2()
+var grounded = false
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
